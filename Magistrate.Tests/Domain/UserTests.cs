8921531bb3e547268bad8ac0141fd8b7714a3a21
andy <andy@andydote.co.uk>
2015-10-24 13:24:27 +0100

    user: add user aggregate and basic functionality

---
+++ Magistrate.Tests/Domain/UserTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Magistrate.Domain;
+using Shouldly;
+using Xunit;
+
+namespace Magistrate.Tests.Domain
+{
+	public class UserTests
+	{
+		[Fact]
+		public void A_user_must_have_a_key()
+		{
+			Should.Throw<ArgumentException>(
+				() => User.Create("", "No key user")).Message
+				.ShouldContain("Key cannot be null or whitespace");
+		}
+
+		[Fact]
+		public void A_user_must_have_a_name()
+		{
+			Should.Throw<ArgumentException>(
+				() => User.Create("some-key", "")).Message
+				.ShouldContain("Name cannot be null or whitespace");
+		}
+
+		[Fact]
+		public void A_user_gets_all_properties_assigned()
+		{
+			var user = User.Create("some-key", "some name");
+
+			user.ShouldSatisfyAllConditions(
+				() => user.ID.ShouldNotBe(Guid.Empty),
+				() => user.Key.ShouldBe("some-key"),
+				() => user.Name.ShouldBe("some name")
+			);
+		}
+
+		[Fact]
+		public void A_users_name_cannot_be_removed()
+		{
+			var user = User.Create("some-key", "some name");
+
+			Should.Throw<ArgumentException>(
+				() => user.ChangeName("")).Message
+				.ShouldContain("Name cannot be null or whitespace");
+		}
+
+		[Fact]
+		public void Changing_a_users_name_works()
+		{
+			var user = User.Create("some-key", "some name");
+
+			user.ChangeName("new name");
+			user.Name.ShouldBe("new name");
+		}
+
+
+	}
+}
---
+++ Magistrate/Domain/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Ledger;
+using Magistrate.Domain.Events;
+
+namespace Magistrate.Domain
+{
+	public class User : AggregateRoot<Guid>
+	{
+		public string Key { get; private set; }
+		public string Name { get; private set; }
+
+		public static User Create(string key, string name)
+		{
+			ValidateKey(key);
+			ValidateName(name);
+
+			var user = new User();
+			user.ApplyEvent(new UserCreatedEvent
+			{
+				ID = Guid.NewGuid(),
+				Key = key,
+				Name = name
+			});
+
+			return user;
+		}
+
+		public void ChangeName(string name)
+		{
+			ValidateName(name);
+			ApplyEvent(new NameChangedEvent { NewName = name });
+		}
+
+		private static void ValidateKey(string key)
+		{
+			if (string.IsNullOrWhiteSpace(key))
+				throw new ArgumentException("Key cannot be null or whitespace", nameof(key));
+		}
+
+		private static void ValidateName(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("Name cannot be null or whitespace", nameof(name));
+		}
+
+
+		private void Handle(UserCreatedEvent e)
+		{
+			ID = e.ID;
+			Key = e.Key;
+			Name = e.Name;
+		}
+
+		private void Handle(NameChangedEvent e)
+		{
+			Name = e.NewName;
+		}
+
+	}
+}
