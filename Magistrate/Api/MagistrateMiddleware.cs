7f5d2218d8cdd168f45df46915a29ca634187807
andy <andy@andydote.co.uk>
2015-10-25 11:38:04 +0000

    api: add a basic routing component for the api

---
+++ Magistrate/Api/MagistrateMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Magistrate.Domain;
+using Owin;
+using Owin.Routing;
+
+namespace Magistrate.Api
+{
+	public class MagistrateTopware
+	{
+		private readonly Store _store;
+
+		public MagistrateTopware(Store store)
+		{
+			_store = store;
+		}
+
+		public void Configure(IAppBuilder app)
+		{
+			app.Route("/api/permissions/all").Get(async context =>
+			{
+				await context.WriteJson(_store.Permissions);
+			});
+		}
+	}
+}
