a36c90c85caeae809194710600c9af9fe5a7f32a
andy <andy@andydote.co.uk>
2015-10-25 12:32:38 +0000

    api: support changing name and description of a permission

--- Magistrate/Api/MagistrateMiddleware.cs
+++ Magistrate/Api/MagistrateMiddleware.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using Magistrate.Domain;
+using Microsoft.Owin;
 using Owin;
 using Owin.Routing;
 
@@ -20,6 +24,69 @@ namespace Magistrate.Api
 			{
 				await context.WriteJson(_store.Permissions.AllPermissions);
 			});
+
+			app.Route("/api/permissions/{permission-key}").Get(async context =>
+			{
+				var permission = GetPermission(context);
+
+				await NotFoundOrAction(context, permission, () => context.WriteJson(permission));
+			});
+
+			app.Route("/api/permissions/{permission-key}/changeName").Put(async context =>
+			{
+				var permission = GetPermission(context);
+
+				await NotFoundOrAction(context, permission, async () =>
+				{
+					permission.ChangeName(ReadBody(context));
+					_store.Save(permission);
+
+					await Task.Yield();
+				});
+			});
+
+			app.Route("/api/permissions/{permission-key}/changeDescription").Put(async context =>
+			{
+				var permission = GetPermission(context);
+
+				await NotFoundOrAction(context, permission, async () =>
+				{
+					permission.ChangeDescription(ReadBody(context));
+					_store.Save(permission);
+
+					await Task.Yield();
+				});
+			});
 		}
+
+		private Permission GetPermission(IOwinContext context)
+		{
+			var key = context.GetRouteValue<string>("permission-key");
+			return _store.Permissions.ByKey(key);
+		}
+
+		private string ReadBody(IOwinContext context)
+		{
+			using (var reader = new StreamReader(context.Request.Body))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		private static async Task NotFoundOrAction<T>(IOwinContext context, T item, Func<Task> action)
+		{
+			if (item == null)
+				context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+			else
+				await action();
+		}
+
+		//private static async Task RespondOrNotFound(IOwinContext context, Permission permission)
+		//{
+		//	if (permission == null)
+		//		context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+		//	else
+		//		await context.WriteJson(permission);
+		//}
 	}
 }
--- Magistrate/ReadModels/PermissionsReadModel.cs
+++ Magistrate/ReadModels/PermissionsReadModel.cs
@@ -7,16 +7,23 @@ namespace Magistrate.ReadModels
 {
 	public class PermissionsReadModel
 	{
-		public Dictionary<Guid, string> AllPermissions { get; }
+		public Dictionary<string, string> AllPermissions => _permissions.ToDictionary(p => p.Key, p => p.Name);
+
+		private readonly HashSet<Permission> _permissions;
 
 		public PermissionsReadModel()
 		{
-			AllPermissions = new Dictionary<Guid, string>();
+			_permissions = new HashSet<Permission>();
 		}
 
 		public void ProjectTo(Permission permission)
 		{
-			AllPermissions[permission.ID] = permission.Name;
+			_permissions.Add(permission);
+		}
+
+		public Permission ByKey(string key)
+		{
+			return _permissions.FirstOrDefault(p => string.Equals(p.Key, key, StringComparison.OrdinalIgnoreCase));
 		}
 	}
 }
