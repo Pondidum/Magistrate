59500eeca55256b60465c603d0651c32ea9ec4e2
andy <andy@andydote.co.uk>
2015-10-25 11:38:18 +0000

    projects: add a sample console app to host an api

---
+++ Sample.Host/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Ledger;
+using Ledger.Stores;
+using Magistrate.Api;
+using Magistrate.Domain;
+using Microsoft.Owin.Hosting;
+
+namespace Sample.Host
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var eventStore = new AggregateStore<Guid>(new InMemoryEventStore<Guid>());
+			var store = new Store(eventStore);
+			var magistrate = new MagistrateTopware(store);
+
+			var permission = Permission.Create("permission-one", "Permission One", "This does something, probably.");
+
+			store.Save(permission);
+
+            var host = WebApp.Start("http://localhost:4444", app =>
+			{
+				magistrate.Configure(app);
+			});
+
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+
+			host.Dispose();
+		}
+	}
+}
