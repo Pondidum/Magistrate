be65ffe9e32fe27cbd4389feb318144c68fe7bd8
andy <andy@andydote.co.uk>
2015-10-28 22:31:55 +0000

    ui: hook up the filterBar to the userView

--- Sample.Host/content/components/filterBar.jsx
+++ Sample.Host/content/components/filterBar.jsx
@@ -1,11 +1,15 @@
 var FilterBar = React.createClass({
 
+  onChange(e) {
+    this.props.filterChanged(e.target.value);
+  },
+
   render() {
 
     return (
-      <div className="row">
+      <div className="row" style={{ marginBottom: "1em" }}>
         <div className="pull-right col-md-5">
-          <input type="text" className="form-control" placeholder="filter..." />
+          <input type="text" className="form-control" placeholder="filter..." onChange={this.onChange} ref="filter" />
         </div>
       </div>
     );
--- Sample.Host/content/components/usersView.jsx
+++ Sample.Host/content/components/usersView.jsx
@@ -2,6 +2,7 @@ var UsersView = React.createClass({
 
   getInitialState() {
     return {
+      filter: "",
       users: []
     };
   },
@@ -28,27 +29,39 @@ var UsersView = React.createClass({
 
   },
 
+  filterChanged(value) {
+    this.setState({
+      filter: value
+    });
+  },
+
   render() {
 
-    var users = this.state.users.map(function(user, index) {
-      return (
-        <div key={index} className="col-md-3">
-          <div className="panel panel-default">
-            <div className="panel-heading">
-              <h3 className="panel-title">{user.name}<span className="glyphicon glyphicon-remove-circle pull-right"></span></h3>
-            </div>
-            <div className="panel-body">
-              <div>Permissions: 15</div>
-              <div>Roles: 3</div>
+    var filter = new RegExp(this.state.filter, "i");
+
+    var users = this.state.users
+      .filter(function(user) {
+        return user.name.search(filter) != -1;
+      })
+      .map(function(user, index) {
+        return (
+          <div key={index} className="col-md-3">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h3 className="panel-title">{user.name}<span className="glyphicon glyphicon-remove-circle pull-right"></span></h3>
+              </div>
+              <div className="panel-body">
+                <div>Permissions: 15</div>
+                <div>Roles: 3</div>
+              </div>
             </div>
           </div>
-        </div>
-      );
-    });
+        );
+      });
 
     return (
       <div>
-        <FilterBar />
+        <FilterBar filterChanged={this.filterChanged} />
         <div className="row">
             {users}
         </div>
