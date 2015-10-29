2e5a8ef5308678d5b885244fdc5da1c3ea03ee36
andy <andy@andydote.co.uk>
2015-10-29 14:49:06 +0000

    ui: action bar buttons are disabled if no one selected

--- Sample.Host/content/components/actionsBar.jsx
+++ Sample.Host/content/components/actionsBar.jsx
@@ -1,12 +1,17 @@
 var ActionsBar = React.createClass({
 
   render() {
+
+    var disabled = this.props.hasUsers
+      ? ""
+      : " disabled";
+
     return (
       <div className={this.props.className}>
         <ul className="list-unstyled list-inline">
           <li><a href="#" className="btn btn-primary">Create User</a></li>
-          <li><a href="#" className="btn btn-default">Add Permission</a></li>
-          <li><a href="#" className="btn btn-default">Add Role</a></li>
+          <li><a href="#" className={"btn btn-default" + disabled}>Add Permission</a></li>
+          <li><a href="#" className={"btn btn-default" + disabled}>Add Role</a></li>
         </ul>
       </div>
     );
