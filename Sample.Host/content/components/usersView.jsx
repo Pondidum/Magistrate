8fc2dc0d555f065c33fb309a502dc32c52dd5416
andy <andy@andydote.co.uk>
2015-10-29 15:51:01 +0000

    ui: remove acitonbar, wrong component level atm

--- Sample.Host/content/components/usersView.jsx
+++ Sample.Host/content/components/usersView.jsx
@@ -44,10 +44,20 @@ var UsersView = React.createClass({
     });
   },
 
+  openPermissions(e) {
+    e.preventDefault();
+    this.refs.permissionsDialog.open()
+  },
+
+  openRoles(e) {
+    e.preventDefault();
+    this.refs.rolesDialog.open();
+  },
+
   render() {
 
     var onUserSelection = this.onUserSelection;
-    var hasSelection = this.state.selected > 0;
+    var noSelection = this.state.selected <= 0 ? " disabled" : "";
     var filter = new RegExp(this.state.filter, "i");
 
     var users = this.state.users
@@ -63,7 +73,23 @@ var UsersView = React.createClass({
     return (
       <div>
         <div className="row" style={{ marginBottom: "1em" }}>
-          <ActionsBar className="col-md-7" hasSelection={hasSelection} />
+          <div className="col-md-7">
+
+            <ul className="list-unstyled list-inline">
+              <li><a href="#" className="btn btn-primary">Create User</a></li>
+              <li><a href="#" className={"btn btn-default" + noSelection} onClick={this.openPermissions}>Add Permission</a></li>
+              <li><a href="#" className={"btn btn-default" + noSelection} onClick={this.openRoles}>Add Role</a></li>
+            </ul>
+
+            <Dialog title="Select Permissions" onSubmit={() => console.log("permissionsDialog.submit") } ref="permissionsDialog">
+              <p>List of all Permissions</p>
+            </Dialog>
+
+            <Dialog title="Select Roles" onSubmit={() => console.log("rolesDialog.submit") } ref="rolesDialog">
+              <p>List of all Roles</p>
+            </Dialog>
+
+          </div>
           <FilterBar className="pull-right col-md-5" filterChanged={this.filterChanged} />
         </div>
         <div className="row">
