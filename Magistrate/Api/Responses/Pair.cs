3ab2c832a49edc5274be1bef133e821388e309e9
andy <andy@andydote.co.uk>
2015-11-02 20:37:08 +0000

    api: fix json casing

--- Magistrate/Api/Responses/Pair.cs
+++ Magistrate/Api/Responses/Pair.cs
@@ -1,8 +1,13 @@
+using Newtonsoft.Json;
+
 namespace Magistrate.Api.Responses
 {
 	public class Pair
 	{
+		[JsonProperty("key")]
 		public string Key { get; set; }
+
+		[JsonProperty("name")]
 		public string Name { get; set; }
 	}
 }
