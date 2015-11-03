92209233a56d0a2f34d90914c6bf5e975a960ac8
andy <andy@andydote.co.uk>
2015-11-03 19:06:49 +0000

    testdata: add 2nd role

--- testdata.sh
+++ testdata.sh
@@ -8,6 +8,13 @@ curl -H 'Content-Type: application/json' -X PUT -d '{"key":"db-cleaner","name":"
 curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/archive-candidate
 curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/restore-candidate
 
+curl -H 'Content-Type: application/json' -X PUT -d '{"key":"admin","name":"Administrator","description":"DO ALL THE THINGS!"}' http://localhost:4444/api/roles
+curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/archive-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/restore-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/create-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/delete-candidate
+
 curl -H 'Content-Type: application/json' -X PUT -d '{"key":"00001","name":"Andy Dote"}' http://localhost:4444/api/users
 curl -d "" -X PUT http://localhost:4444/api/users/00001/addRole/db-cleaner
+curl -d "" -X PUT http://localhost:4444/api/users/00001/addRole/admin
 curl -d "" -X PUT http://localhost:4444/api/users/00001/addPermission/create-candidate
