4236e458a67cf00b7ba90ec4f166fd945576c221
andy <andy@andydote.co.uk>
2015-11-03 20:03:11 +0000

    testData: helps if you add permissions to the right role...

--- testdata.sh
+++ testdata.sh
@@ -9,10 +9,10 @@ curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/archi
 curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/restore-candidate
 
 curl -H 'Content-Type: application/json' -X PUT -d '{"key":"admin","name":"Administrator","description":"DO ALL THE THINGS!"}' http://localhost:4444/api/roles
-curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/archive-candidate
-curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/restore-candidate
-curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/create-candidate
-curl -d "" -X PUT http://localhost:4444/api/roles/db-cleaner/addPermission/delete-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/admin/addPermission/archive-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/admin/addPermission/restore-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/admin/addPermission/create-candidate
+curl -d "" -X PUT http://localhost:4444/api/roles/admin/addPermission/delete-candidate
 
 curl -H 'Content-Type: application/json' -X PUT -d '{"key":"00001","name":"Andy Dote"}' http://localhost:4444/api/users
 curl -d "" -X PUT http://localhost:4444/api/users/00001/addRole/db-cleaner
