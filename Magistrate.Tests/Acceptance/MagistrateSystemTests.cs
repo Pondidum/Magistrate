6b042c48ca680d4b4ebf543ed7ea57ce2cf38424
andy <andy@andydote.co.uk>
2015-11-12 20:56:43 +0000

    system: add a base aggregate for the entire system

---
+++ Magistrate.Tests/Acceptance/MagistrateSystemTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ledger;
+using Ledger.Stores;
+using Magistrate.Domain;
+using Shouldly;
+using Xunit;
+
+namespace Magistrate.Tests.Acceptance
+{
+	public class MagistrateSystemTests
+	{
+		private static readonly MagistrateUser CurrentUser = new MagistrateUser();
+
+		[Fact]
+		public void When_adding_and_removing_permission()
+		{
+			var store = new AggregateStore<Guid>(new InMemoryEventStore<Guid>());
+			var system = new MagistrateSystem(store);
+
+			var permission = Permission.Create(CurrentUser, "first", "First Permission", "");
+
+			system.AddPermission(permission);
+			system.Permissions.ShouldContain(p => p.Key == "first");
+
+			system.RemovePermission(permission);
+			system.Permissions.ShouldBeEmpty();
+		}
+	}
+}
---
+++ Magistrate/Domain/MagistrateSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Ledger;
+using Magistrate.Domain.Events;
+
+namespace Magistrate.Domain
+{
+	public class MagistrateSystem : AggregateRoot<Guid>
+	{
+
+		public IEnumerable<Permission> Permissions => _permissions;
+		 
+		private readonly HashSet<Permission> _permissions;
+
+		private readonly AggregateStore<Guid> _store;
+
+		public MagistrateSystem(AggregateStore<Guid> store)
+		{
+			_store = store;
+			_permissions = new HashSet<Permission>();
+		}
+
+		public void AddPermission(Permission permission)
+		{
+			_store.Save(permission);
+
+			ApplyEvent(new PermissionAddedEvent
+			{
+				PermissionID = permission.ID
+			});
+		}
+
+		public void RemovePermission(Permission permission)
+		{
+			ApplyEvent(new PermissionRemovedEvent
+			{
+				PermissionID = permission.ID
+			});
+		}
+
+
+
+
+		private void Handle(PermissionAddedEvent e)
+		{
+			_permissions.Add(_store.Load(e.PermissionID, Permission.Blank));
+		}
+
+		private void Handle(PermissionRemovedEvent e)
+		{
+			_permissions.RemoveWhere(p => p.ID == e.PermissionID);
+		}
+
+	}
+}
