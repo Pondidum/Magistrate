8b831dbce834cd7ad08e1f34cc8faa306a34e5b4
andy <andy@andydote.co.uk>
2015-11-14 15:38:20 +0000

    gulp: read a base assemblyVersion and update to create AsseblyVersion.cs

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require("gulp");
 var shell = require("gulp-shell");
 var args = require('yargs').argv;
+var fs = require("fs");
+var assemblyInfo = require('gulp-dotnet-assembly-info');
+var rename = require('gulp-rename');
 
+var project = JSON.parse(fs.readFileSync("./package.json"));
 
 var config = {
-  name: "Magistrate",
+  name: project.name,
+  version: project.version,
   mode: args.mode || "Debug",
   commit: process.env.APPVEYOR_REPO_COMMIT || "0",
+  buildNumber: process.env.APPVEYOR_BUILD_VERSION || "0",
   output: "./build/deploy"
 }
 
-
-gulp.task("default", [ "restore" ]);
+gulp.task("default", [ "restore", "version" ], function() {
+  console.log(config.name, config.version);
+});
 
 gulp.task("ci", []);
 
@@ -20,4 +27,16 @@ gulp.task('restore', function() {
   return gulp
     .src(config.name + '.sln', { read: false })
     .pipe(shell('"./tools/nuget/nuget.exe" restore '));
+});
+
+gulp.task('version', function() {
+  return gulp
+    .src(config.name + '/Properties/AssemblyVersion.base')
+    .pipe(rename("AssemblyVersion.cs"))
+    .pipe(assemblyInfo({
+      version: config.version,
+      fileVersion: config.version,
+      description: "Build: " +  config.buildNumber + ", Sha: " + config.commit
+    }))
+    .pipe(gulp.dest('./' + config.name + '/Properties'));
 });
