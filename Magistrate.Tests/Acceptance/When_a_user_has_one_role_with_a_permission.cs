3c1b1af5dd872fc6fcab7339b8c3a66a7b0cc543
andy <andy@andydote.co.uk>
2015-11-15 21:39:20 +0000

    user: update for next set of acceptance tests

--- Magistrate.Tests/Acceptance/When_a_user_has_one_role_with_a_permission.cs
+++ Magistrate.Tests/Acceptance/When_a_user_has_one_role_with_a_permission.cs
@@ -1,31 +1,86 @@
+using Shouldly;
 using Xunit;
 
 namespace Magistrate.Tests.Acceptance
 {
-	public class When_a_user_has_one_role_with_a_permission
+	public class When_a_user_has_one_role_with_a_permission : UserAcceptanceBase
 	{
-		[Fact]
-		public void AddInclude_a_different_permission_adds_to_the_includes_collection() { }
+		public When_a_user_has_one_role_with_a_permission()
+		{
+			TestRole.AddPermission(new MagistrateUser(), FirstPermission);
+			User.AddRole(new MagistrateUser(), TestRole);
+		}
 
 		[Fact]
-		public void AddInclude_the_same_permission_does_nothing() { }
+		public void AddInclude_a_different_permission_adds_to_the_includes_collection()
+		{
+			User.AddInclude(new MagistrateUser(), SecondPermission);
+
+			User.Includes.ShouldBe(SecondPermissionOnly);
+			User.Revokes.ShouldBeEmpty();
+		}
+
+		[Fact(Skip = "Not sure on whether this is the right behaviour yet")]
+		public void AddInclude_the_same_permission_does_nothing()
+		{
+			User.AddInclude(new MagistrateUser(), FirstPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBeEmpty();
+		}
 
 		[Fact]
-		public void RemoveInclude_a_different_permission_does_nothing() { }
+		public void RemoveInclude_a_different_permission_does_nothing()
+		{
+			User.RemoveInclude(new MagistrateUser(), SecondPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBeEmpty();
+		}
 
 		[Fact]
-		public void RemoveInclude_the_same_permission_does_nothing() { }
+		public void RemoveInclude_the_same_permission_does_nothing()
+		{
+			User.RemoveInclude(new MagistrateUser(), FirstPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBeEmpty();
+		}
 
 		[Fact]
-		public void AddRevoke_a_different_permission_adds_to_the_revokes_collection() { }
+		public void AddRevoke_a_different_permission_adds_to_the_revokes_collection()
+		{
+			User.AddRevoke(new MagistrateUser(), SecondPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBe(SecondPermissionOnly);
+		}
 
 		[Fact]
-		public void AddRevoke_the_same_permission_adds_to_the_revokes_collection() { }
+		public void AddRevoke_the_same_permission_adds_to_the_revokes_collection()
+		{
+			User.AddRevoke(new MagistrateUser(), FirstPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBe(FirstPermissionOnly);
+		}
 
 		[Fact]
-		public void RemoveRevoke_a_different_permission_does_nothing() { }
+		public void RemoveRevoke_a_different_permission_does_nothing()
+		{
+			User.RemoveRevoke(new MagistrateUser(), SecondPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBeEmpty();
+		}
 
 		[Fact]
-		public void RemoveRevoke_the_same_permission_does_nothing() { }
+		public void RemoveRevoke_the_same_permission_does_nothing()
+		{
+			User.RemoveRevoke(new MagistrateUser(), FirstPermission);
+
+			User.Includes.ShouldBeEmpty();
+			User.Revokes.ShouldBeEmpty();
+		}
 	}
 }
