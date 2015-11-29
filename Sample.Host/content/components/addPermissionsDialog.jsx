7836c6b26f9280dac90065abba94eca776df98cd
andy <andy@andydote.co.uk>
2015-11-29 16:02:43 +0000

    ui: addPermissionDialog: add table of permissions

--- Sample.Host/content/components/addPermissionsDialog.jsx
+++ Sample.Host/content/components/addPermissionsDialog.jsx
@@ -1,19 +1,75 @@
+var Input = ReactBootstrap.Input;
+
 var AddPermissionsDialog = React.createClass({
+
+  getInitialState() {
+    return {
+      permissions: []
+    };
+  },
 
   showDialog(e) {
     e.preventDefault();
+    this.getPermissions();
     this.refs.dialog.open();
+  },
+
+  getPermissions() {
+
+    var url = "/api/permissions/all";
+
+    $.ajax({
+      url: url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({
+          permissions: data || []
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(url, status, err.toString());
+      }.bind(this)
+    });
+
   },
 
   render() {
 
     var noSelection = this.props.noSelection;
 
+    var permissions = this.state.permissions.map(function(perm, index) {
+      return (
+        <tr key={index}>
+          <td>
+            <div className="checkbox">
+              <label>
+                <input type="checkbox" /><span className="checkbox-material"><span className="check"></span></span>
+              </label>
+            </div>
+          </td>
+          <td>{perm.name}</td>
+          <td>{perm.description}</td>
+        </tr>
+      );
+    });
+
     return (
       <a href="#" className={"btn btn-raised btn-default" + (noSelection ? " disabled" : "")} onClick={this.showDialog}>
         Add Permission
         <Dialog title="Select Permissions" onSubmit={() => console.log("permissionsDialog.submit") } ref="dialog">
-          <p>List of all Permissions</p>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th className="col-sm-1">#</th>
+                <th className="col-sm-4">Name</th>
+                <th className="col-sm-7">Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {permissions}
+            </tbody>
+          </table>
         </Dialog>
       </a>
     );
