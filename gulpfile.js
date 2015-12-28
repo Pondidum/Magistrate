69a9c32885aa494138717ab91a726748c6bee8a9
andy <andy@andydote.co.uk>
2015-12-28 20:02:19 +0000

    tests: use xunit.runner.console nuget package for tests

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('test', [ "compile" ], function() {
   return gulp
     .src(['**/bin/*/*.Tests.dll'], { read: false })
     .pipe(xunit({
-      executable: './tools/xunit/xunit.console.exe',
+      executable: './packages/xunit.runner.console.2.1.0/tools/xunit.console.exe',
       options: {
         verbose: true,
         nologo: true,
