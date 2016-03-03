85dc2e478620fbfcc075d4aed8d46f9091de4a04
andy <andy@andydote.co.uk>
2016-03-03 23:28:42 +0000

    userOverview: remove unused divs

--- Magistrate/client/components/users/userOverview.js
+++ Magistrate/client/components/users/userOverview.js
@@ -7,27 +7,23 @@ import Overview from '../overview'
 
 
 const SimpleGrid = ({ children }) => (
-  <div>
-    <div className="row">
-      <ul className="list-unstyled list-inline col-sm-12">
-        {children}
-      </ul>
-    </div>
+  <div className="row">
+    <ul className="list-unstyled list-inline col-sm-12">
+      {children}
+    </ul>
   </div>
 );
 
 const SimpleOverview = ({ users }) => (
   <div>
-    <div>
-      <div className="row">
-        <div className="col-sm-2">
-          <CreateUserDialog  />
-        </div>
+    <div className="row">
+      <div className="col-sm-2">
+        <CreateUserDialog  />
       </div>
-      <SimpleGrid>
-        {users.map((user, i) => <UserTile key={i} content={user} /> )}
-      </SimpleGrid>
     </div>
+    <SimpleGrid>
+      {users.map((user, i) => <UserTile key={i} content={user} /> )}
+    </SimpleGrid>
   </div>
 );
 
@@ -41,21 +37,3 @@ const mapStateToProps = (state) => {
 const UserOverview = connect(mapStateToProps)(SimpleOverview);
 
 export default UserOverview
-
-// var UserOverview = React.createClass({
-//
-//   render() {
-//
-//     return (
-//       <Overview
-//         tile={UserTile}
-//         create={CreateUserDialog}
-//         collection={this.props.collection}
-//         {...this.props}
-//       />
-//     );
-//
-//   }
-// });
-//
-// export default UserOverview
