4ef9ece825855377df82cd6486e1ecd651108288
andy <andy@andydote.co.uk>
2016-03-03 23:25:15 +0000

    userOverview: update to read from store

--- Magistrate/client/components/users/userOverview.js
+++ Magistrate/client/components/users/userOverview.js
@@ -1,22 +1,61 @@
 import React from 'react'
+import { connect } from 'react-redux'
+
 import UserTile from './usertile'
 import CreateUserDialog from './CreateUserDialog'
 import Overview from '../overview'
 
-var UserOverview = React.createClass({
 
-  render() {
+const SimpleGrid = ({ children }) => (
+  <div>
+    <div className="row">
+      <ul className="list-unstyled list-inline col-sm-12">
+        {children}
+      </ul>
+    </div>
+  </div>
+);
 
-    return (
-      <Overview
-        tile={UserTile}
-        create={CreateUserDialog}
-        collection={this.props.collection}
-        {...this.props}
-      />
-    );
+const SimpleOverview = ({ users }) => (
+  <div>
+    <div>
+      <div className="row">
+        <div className="col-sm-2">
+          <CreateUserDialog  />
+        </div>
+      </div>
+      <SimpleGrid>
+        {users.map((user, i) => <UserTile key={i} content={user} /> )}
+      </SimpleGrid>
+    </div>
+  </div>
+);
 
+
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
   }
-});
+}
+
+const UserOverview = connect(mapStateToProps)(SimpleOverview);
 
 export default UserOverview
+
+// var UserOverview = React.createClass({
+//
+//   render() {
+//
+//     return (
+//       <Overview
+//         tile={UserTile}
+//         create={CreateUserDialog}
+//         collection={this.props.collection}
+//         {...this.props}
+//       />
+//     );
+//
+//   }
+// });
+//
+// export default UserOverview
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    //new webpack.HotModuleReplacementPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
