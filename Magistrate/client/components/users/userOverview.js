538bafb4384ba49312cb809fe72816f3b426225d
andy <andy@andydote.co.uk>
2016-03-03 23:47:47 +0000

    userOverview: change to a class so we can use state for the filter

--- Magistrate/client/components/users/userOverview.js
+++ Magistrate/client/components/users/userOverview.js
@@ -1,25 +1,50 @@
 import React from 'react'
+import { Component } from 'react'
+
 import { connect } from 'react-redux'
 
 import UserTile from './usertile'
 import CreateUserDialog from './CreateUserDialog'
 import Overview from '../overview'
+import FilterBar from '../filterBar'
 
-const SimpleOverview = ({ users }) => (
-  <div>
-    <div className="row">
-      <div className="col-sm-2">
-        <CreateUserDialog  />
+//const SimpleOverview = ({ users }) => (
+class SimpleOverview extends Component {
+
+  constructor() {
+    super();
+    this.state = { filter: '' };
+  }
+
+  render() {
+    return (
+    <div>
+      <div className="row">
+        <div className="col-sm-2">
+          <CreateUserDialog  />
+        </div>
+        <FilterBar filterChanged={value => this.setState({ filter: value })} />
+      </div>
+      <div className="row">
+        <ul className="list-unstyled list-inline col-sm-12">
+          {this.props.users
+            .filter(user => {
+              var exp = new RegExp(this.state.filter, "i");
+              var isName =  user.name.search(exp) != -1;
+              var isDescription = (user.description || "").search(exp) != -1;
+
+              return isName || isDescription;
+            })
+            .map((user, i) => (
+              <UserTile key={i} content={user} />
+            ))
+          }
+        </ul>
       </div>
     </div>
-    <div className="row">
-      <ul className="list-unstyled list-inline col-sm-12">
-        {users.map((user, i) => <UserTile key={i} content={user} /> )}
-      </ul>
-    </div>
-  </div>
-);
-
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
