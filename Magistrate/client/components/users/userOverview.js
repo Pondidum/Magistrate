c2ab50fa7dc5ddcb091fcb943c55d7c31a073fc7
andy <andy@andydote.co.uk>
2016-03-03 23:50:21 +0000

    userOverview: refactor filtering to me more efficient

--- Magistrate/client/components/users/userOverview.js
+++ Magistrate/client/components/users/userOverview.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux'
 import UserTile from './usertile'
 import CreateUserDialog from './CreateUserDialog'
 import Overview from '../overview'
-import FilterBar from '../filterBar'
+import FilterBar from '../filterbar'
 
 //const SimpleOverview = ({ users }) => (
 class SimpleOverview extends Component {
@@ -17,31 +17,33 @@ class SimpleOverview extends Component {
   }
 
   render() {
-    return (
-    <div>
-      <div className="row">
-        <div className="col-sm-2">
-          <CreateUserDialog  />
-        </div>
-        <FilterBar filterChanged={value => this.setState({ filter: value })} />
-      </div>
-      <div className="row">
-        <ul className="list-unstyled list-inline col-sm-12">
-          {this.props.users
-            .filter(user => {
-              var exp = new RegExp(this.state.filter, "i");
-              var isName =  user.name.search(exp) != -1;
-              var isDescription = (user.description || "").search(exp) != -1;
 
-              return isName || isDescription;
-            })
-            .map((user, i) => (
-              <UserTile key={i} content={user} />
-            ))
-          }
-        </ul>
+    var exp = new RegExp(this.state.filter, "i");
+    var userTiles = this.props.users
+      .filter(user => {
+        var isName =  user.name.search(exp) != -1;
+        var isDescription = (user.description || "").search(exp) != -1;
+
+        return isName || isDescription;
+      })
+      .map((user, i) => (
+        <UserTile key={i} content={user} />
+      ));
+
+    return (
+      <div>
+        <div className="row">
+          <div className="col-sm-2">
+            <CreateUserDialog  />
+          </div>
+          <FilterBar filterChanged={value => this.setState({ filter: value })} />
+        </div>
+        <div className="row">
+          <ul className="list-unstyled list-inline col-sm-12">
+            {userTiles}
+          </ul>
+        </div>
       </div>
-    </div>
     )
   }
 }
