e25074449b2fa6274d11a169039c4249382a5770
andy <andy@andydote.co.uk>
2016-03-08 23:33:32 +0000

    client: add remote middleware

--- Magistrate/client/index.js
+++ Magistrate/client/index.js
@@ -5,6 +5,7 @@ import { render } from 'react-dom'
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
+import remoteMiddleware from './infrastructure/remoteMiddleware'
 
 import { setState } from './actions'
 import rootReducer from './reducers'
@@ -16,7 +17,8 @@ var socket = new WebSocket(`ws://${location.hostname}:8090`);
 socket.onopen = () => console.log("opened");
 socket.onclose = () => console.log("closed");
 
-const store = createStore(rootReducer);
+const createStoreWithMiddleware = applyMiddleware(remoteMiddleware(socket))(createStore);
+const store = createStoreWithMiddleware(rootReducer);
 
 socket.onmessage = (e) => {
   var state = JSON.parse(e.data);
---
+++ Magistrate/client/infrastructure/remoteMiddleware.js
@@ -0,0 +1,10 @@
+const remoteMiddleware = socket => store => next => action => {
+
+  if (action.meta && action.meta.remote)
+    socket.send(JSON.stringify(action));
+
+
+  next(action);
+}
+
+export default remoteMiddleware
