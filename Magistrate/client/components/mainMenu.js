561c918ebee996a8ff3f1ddda0008e514d6d77f3
andy <andy@andydote.co.uk>
2016-04-12 21:17:27 +0100

    mainMenu: make stateless

--- Magistrate/client/components/mainMenu.js
+++ Magistrate/client/components/mainMenu.js
@@ -2,44 +2,33 @@ import React from 'react'
 import Tile from './tile'
 import TileSizeSelector from './TileSizeSelector'
 
-var MainMenu = React.createClass({
+var MainMenu = ({ navigate, selected }) => {
 
-  onTabSelect(key) {
-    this.props.navigate(key);
-  },
+  const items = [ "Users", "Roles", "Permissions", "History" ];
 
-  render() {
+  const tabs = items.map(function(item, index) {
 
-    var navigate = this.props.navigate;
-    var selected = this.props.selected;
+    var onClick = function(e) {
+      e.preventDefault();
+      navigate(item.toLowerCase());
+    }
 
-    var items = [ "Users", "Roles", "Permissions", "History" ];
-
-    var tabs = items.map(function(item, index) {
-
-      var onClick = function(e) {
-        e.preventDefault();
-        navigate(item.toLowerCase());
-      }
-
-      var active = selected == item.toLowerCase() ? "active" : "";
-
-      return (
-        <li key={index} className={active}>
-          <a href="#" onClick={onClick}>{item}</a>
-        </li>
-      );
-
-    });
+    var active = selected == item.toLowerCase() ? "active" : "";
 
     return (
-      <ul className="nav nav-tabs">
-        {tabs}
-        <TileSizeSelector className="pull-right" />
-      </ul>
+      <li key={index} className={active}>
+        <a href="#" onClick={onClick}>{item}</a>
+      </li>
     );
-  }
 
-});
+  });
+
+  return (
+    <ul className="nav nav-tabs">
+      {tabs}
+      <TileSizeSelector className="pull-right" />
+    </ul>
+  );
+}
 
 export default MainMenu
