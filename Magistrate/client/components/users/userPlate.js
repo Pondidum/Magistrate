b964f16b103a4af921dba2cc70fa3818e84ffa02
andy <andy@andydote.co.uk>
2016-04-15 22:24:41 +0100

    userPlate: support navigation to singleUserView

--- Magistrate/client/components/users/userPlate.js
+++ Magistrate/client/components/users/userPlate.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { routeActions } from 'react-router-redux'
+
 import Plate from '../plate'
 import DeleteUserDialog from './DeleteUserDialog'
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (key) => dispatch(routeActions.push(`/users/${key}`))
+  }
+}
 
 class UserPlate extends Component {
 
   render () {
-    const { content } = this.props;
+    const { content, navigate } = this.props;
     return (
       <Plate
         title={content.name}
         additions={<DeleteUserDialog user={content} ref={d => this.dialog = d} />}
+        onClick={e => { e.preventDefault(); navigate(content.key); }}
         onCross={() => this.dialog.getWrappedInstance().open()}>
         <span>Roles: {content.roles.length}</span>
         <span>Includes: {content.includes.length}</span>
@@ -19,4 +29,4 @@ class UserPlate extends Component {
   }
 }
 
-export default UserPlate
+export default  connect(null, mapDispatchToProps)(UserPlate)
