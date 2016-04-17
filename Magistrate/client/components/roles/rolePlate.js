c32850782065d9de377aa1334c35bbd6c7e8d617
andy <andy@andydote.co.uk>
2016-04-17 15:26:39 +0100

    roleTile: replace with rolePlate

---
+++ Magistrate/client/components/roles/rolePlate.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { routeActions } from 'react-router-redux'
+
+import Plate from '../Plate'
+import DeleteRoleDialog from './DeleteRoleDialog'
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (key) => dispatch(routeActions.push(`/user/${key}`))
+  }
+}
+
+class RolePlate extends Component {
+
+  render() {
+    const { content, navigate } = this.props;
+
+    return (
+      <Plate
+        title={content.name}
+        additions={<DeleteRoleDialog role={content} ref={d => this.dialog = d} />}
+        onClick={e => { e.preventDefault(); navigate(content.key); }}
+        onCross={() => this.dialog.getWrappedInstance().open()}>
+        {content.description}
+      </Plate>
+    )
+  }
+}
+
+export default connect(null, mapDispatchToProps)(RolePlate)
