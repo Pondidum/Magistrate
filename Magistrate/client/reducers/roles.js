6688468af68084a5f5dfe164559576b3e714c437
andy <andy@andydote.co.uk>
2016-04-17 15:39:19 +0100

    reduders: handler delete_role

--- Magistrate/client/reducers/roles.js
+++ Magistrate/client/reducers/roles.js
@@ -1,8 +1,12 @@
 const roles = (state = [], action) => {
 
   switch (action.type) {
+
     case "COLLECTIONS_CHANGED":
       return action.roles;
+
+    case "DELETE_ROLE":
+      return state.filter(r => r.key != action.key);
 
     default:
       return state;
