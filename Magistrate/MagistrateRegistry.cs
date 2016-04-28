17df3a4ca21bebd96018188031e284add0ef5c9f
andy <andy@andydote.co.uk>
2016-04-29 00:04:33 +0100

    container: add registry and service policy

---
+++ Magistrate/MagistrateRegistry.cs
@@ -0,0 +1,33 @@
+using Ledger;
+using Magistrate.Infrastructure;
+using MediatR;
+using StructureMap;
+using StructureMap.Graph;
+
+namespace Magistrate
+{
+	public class MagistrateRegistry : Registry
+	{
+		public MagistrateRegistry(IEventStore store)
+		{
+			Scan(a =>
+			{
+				a.TheCallingAssembly();
+				a.WithDefaultConventions();
+
+				a.AddAllTypesOf(typeof(IRequestHandler<,>));
+				a.AddAllTypesOf(typeof(INotificationHandler<>));
+			});
+
+			For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+			For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+			For<IMediator>().Use<Mediator>();
+
+			Policies.Add<ServicePolicy>();
+
+			For<Projectionist>().Singleton();
+			For<IEventStore>()
+				.Use(context => new ProjectionStore(store, context.GetInstance<Projectionist>().Apply));
+		}
+	}
+}
---
+++ Magistrate/ServicePolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using StructureMap;
+using StructureMap.Pipeline;
+
+namespace Magistrate
+{
+	public class ServicePolicy : IInstancePolicy
+	{
+		public void Apply(Type pluginType, Instance instance)
+		{
+			if (instance.ReturnedType.Name.EndsWith("Service"))
+				instance.SetLifecycleTo<SingletonLifecycle>();
+		}
+	}
+}
