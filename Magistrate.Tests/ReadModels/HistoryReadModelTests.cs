44bde7182ebac9b2475864bab3bab1a7aac3fe27
andy <andy@andydote.co.uk>
2016-05-07 11:32:43 +0100

    historyReadModel: start on projections

---
+++ Magistrate.Tests/ReadModels/HistoryReadModelTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Magistrate.Domain;
+using Magistrate.Domain.Events.PermissionEvents;
+using Magistrate.ReadModels;
+using Shouldly;
+using Xunit;
+
+namespace Magistrate.Tests.ReadModels
+{
+	public class HistoryReadModelTests
+	{
+		private readonly HistoryReadModel _model;
+		private Operator _user1;
+		private Operator _user2;
+
+		public HistoryReadModelTests()
+		{
+			_model = new HistoryReadModel();
+
+			_user1 = new Operator { Name = "Andy Dote" };
+			_user2 = new Operator { Name = "John Wick" };
+		}
+
+		[Fact]
+		public void When_a_permission_is_created()
+		{
+			_model.Project(new PermissionCreatedEvent(_user1, Guid.NewGuid(), new PermissionKey("p1"), "First Perm", "Does something"));
+
+			_model.Entries.Single().ShouldBe($"Permission 'First Perm' created by ${_user1.Name}");
+		}
+
+		[Fact]
+		public void When_a_permission_is_renamed()
+		{
+			var id = Guid.NewGuid();
+			_model.Project(new PermissionCreatedEvent(_user1, id, new PermissionKey("p1"), "First Perm", "Does something"));
+			_model.Project(new PermissionNameChangedEvent(_user2, "First Permission") { AggregateID = id });
+
+			var entries = _model.Entries.ToList();
+
+			entries[0].ShouldBe($"Permission 'First Perm' created by ${_user1.Name}");
+			entries[1].ShouldBe($"Permission Name changed from 'First Perm' to 'First Permission' by {_user2.Name}");
+		}
+
+		[Fact]
+		public void When_a_permissions_description_is_changed()
+		{
+			var id = Guid.NewGuid();
+			_model.Project(new PermissionCreatedEvent(_user1, id, new PermissionKey("p1"), "First Perm", "Does something"));
+			_model.Project(new PermissionDescriptionChangedEvent(_user2, "Does things") { AggregateID = id });
+
+			var entries = _model.Entries.ToList();
+
+			entries[0].ShouldBe($"Permission 'First Perm' created by ${_user1.Name}");
+			entries[1].ShouldBe($"Permission Description changed from 'Does something' to 'Does things' by {_user2.Name}");
+		}
+	}
+}
---
+++ Magistrate/ReadModels/HistoryReadModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Ledger;
+using Magistrate.Domain.Events.PermissionEvents;
+using Magistrate.Infrastructure;
+
+namespace Magistrate.ReadModels
+{
+	public class HistoryReadModel
+	{
+		public IEnumerable<string> Entries => _entries;
+		 
+		private readonly Projector _projections;
+		private readonly List<string> _entries;
+
+		public HistoryReadModel()
+		{
+			_projections = new Projector();
+
+			var permissions = new Dictionary<Guid, PermissionDto>();
+
+			_entries = new List<string>();
+
+			_projections.Register<PermissionCreatedEvent>(e =>
+			{
+				permissions[e.ID] = new PermissionDto { Name = e.Name, Description = e.Description };
+				_entries.Add($"Permission '{e.Name}' created by ${e.Operator.Name}");
+			});
+
+			_projections.Register<PermissionNameChangedEvent>(e =>
+			{
+				_entries.Add($"Permission Name changed from '{permissions[e.AggregateID].Name}' to '{e.NewName}' by {e.Operator.Name}");
+			});
+
+			_projections.Register<PermissionDescriptionChangedEvent>(e =>
+			{
+				_entries.Add($"Permission Description changed from '{permissions[e.AggregateID].Description}' to '{e.NewDescription}' by {e.Operator.Name}");
+			});
+		}
+
+		public void Project(DomainEvent<Guid> e)
+		{
+			_projections.Apply(e);
+		}
+
+
+		public class PermissionDto
+		{
+			public string Name { get; set; }
+			public string Description { get; set; }
+		}
+	}
+}
