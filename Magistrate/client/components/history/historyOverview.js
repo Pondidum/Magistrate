e5f44e65006df48738dcd65f580e37b53adc435f
andy <andy@andydote.co.uk>
2016-05-20 22:55:48 +0100

    history: add page links

--- Magistrate/client/components/history/historyOverview.js
+++ Magistrate/client/components/history/historyOverview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
 
+import { Link } from 'react-router'
 import FilterBar from '../filterbar'
 import HistoryRow from './HistoryRow'
 
@@ -17,7 +18,7 @@ const HistoryOverview = ({ history, page }) => {
   const pageSize = 10;
   const current = moment();
 
-  const totalPages = Math.floor(history.length / pageSize);
+  const totalPages = Math.ceil(history.length / pageSize);
   const start = page * pageSize;
   const end = start + pageSize;
 
@@ -26,6 +27,10 @@ const HistoryOverview = ({ history, page }) => {
   }).slice(start, end);
 
   var links = [];
+
+  for (var i = 0; i < totalPages; i++) {
+    links.push(<li key={i}><Link to={"/history/" + i}>{i + 1}</Link></li>);
+  }
 
   return (
     <div>
@@ -51,83 +56,3 @@ const HistoryOverview = ({ history, page }) => {
 }
 
 export default connect(mapStateToProps)(HistoryOverview)
-/*
-var HistoryOverview = React.createClass({
-
-  getInitialState() {
-    return {
-      history: [],
-      pageSize: 10,
-      currentPage: 0
-    };
-  },
-
-  componentDidMount() {
-    this.getHistory();
-  },
-
-  getHistory() {
-    $.ajax({
-      url: "/api/history/all",
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({
-          history: data || []
-        });
-      }.bind(this)
-    });
-  },
-
-  onFilterChanged(value) {
-  },
-
-  onChangePage(index, e) {
-    e.preventDefault();
-    this.setState({ currentPage: index });
-  },
-
-  render() {
-    var self = this;
-    var current = moment();
-
-    var totalPages = Math.floor(this.state.history.length / this.state.pageSize);
-    var start = this.state.currentPage * this.state.pageSize;
-    var end = start + this.state.pageSize;
-    var links = [];
-
-    var history = this.state.history.map(function(item, index) {
-      return (<HistoryRow key={index} history={item} current={current} index={index}/>);
-    }).slice(start, end);
-
-    for (var i = 0; i < totalPages; i++) {
-      links.push(<li key={i}><a href="#" onClick={self.onChangePage.bind(self, i)} >{i + 1}</a></li>);
-    }
-
-    return (
-      <div>
-        <div>
-          <div className="row">
-            <FilterBar filterChanged={this.onFilterChanged} />
-          </div>
-          <div className="row">
-            <ul className="list-unstyled col-sm-12">
-              {history}
-            </ul>
-          </div>
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <ul className="pagination">
-                {links}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-});
-
-export default HistoryOverview
-*/
